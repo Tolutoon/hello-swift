2d69642380a34d15774e721d74cb70356292d0c0
Tolu <getolopadetolu@gmail.com>
2026-02-15 10:56:40 +0100

    Initial Commit

---
+++ hello-swift/hello_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  hello_swiftApp.swift
+//  hello-swift
+//
+//  Created by TOLU on 15/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct hello_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ hello-swiftTests/hello_swiftTests.swift
@@ -0,0 +1,17 @@
+//
+//  hello_swiftTests.swift
+//  hello-swiftTests
+//
+//  Created by TOLU on 15/02/2026.
+//
+
+import Testing
+@testable import hello_swift
+
+struct hello_swiftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
