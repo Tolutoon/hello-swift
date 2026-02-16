e00d81f0ff25637d473a248b30b08dab566c4c62
Tolu <getolopadetolu@gmail.com>
2026-02-16 14:46:43 +0100

    in progress

---
+++ hello-swift/HorizontalListView.swift
@@ -0,0 +1,18 @@
+//
+//  HorizontalListView.swift
+//  hello-swift
+//
+//  Created by TOLU on 16/02/2026.
+//
+
+import SwiftUI
+
+struct HorizontalListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HorizontalListView()
+}
